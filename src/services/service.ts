af2510eab5a5f8161b4676c7af090030495353b8
alena <108529764+AlenaMushko@users.noreply.github.com>
2023-09-17 21:45:21 +0300

    add: intercepter

--- src/services/service.ts
+++ src/services/service.ts
@@ -4,7 +4,14 @@ import {baseURL} from "../constants/urls";
 
 type IRes<U> = Promise<AxiosResponse<U>>
 export const service = axios.create({baseURL});
+service.interceptors.request.use(req => {
+    const accessToken = sessionStorage.getItem('userName')
 
+    if (accessToken) {
+        req.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return req;
+})
 
 export type {
     IRes
